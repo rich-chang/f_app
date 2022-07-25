42719e824338d548ad80d9d8719b3e13462c1ea0
Rich Chang <4301176+rich-chang@users.noreply.github.com>
2022-07-25 23:43:24 +0800

    Implement splash page ui layout

---
+++ lib/screen/splash.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+            child: Image.asset('images/splash.png')
+      ),
+    );// This trailing comma makes auto-formatting nicer for build methods.
+  }
+}
