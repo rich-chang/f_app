b72ea0dc11dcf8151478842c844c9fee7396d640
Rich Chang <app.developer.rich@gmail.com>
2022-08-09 14:05:29 +0800

    Add agreement title

--- lib/screen/signup/agreement_page.dart
+++ lib/screen/signup/agreement_page.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 class AgreementPage extends StatefulWidget {
   const AgreementPage({Key? key}) : super(key: key);
 
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
   @override
   State<AgreementPage> createState() => _AgreementPageState();
 }
@@ -31,6 +39,7 @@ class _AgreementPageState extends State<AgreementPage> {
             child: Text(
               '<  返回',
               style: TextStyle(
+                color: Colors.white,
                 fontFamily: 'SF Pro Text',
                 fontSize: 17,
                 fontWeight: FontWeight.w400,
@@ -40,12 +49,28 @@ class _AgreementPageState extends State<AgreementPage> {
           title: const Text(
             '註冊',
             style: TextStyle(
+              color: Colors.white,
               fontFamily: 'SF Pro Text',
               fontSize: 17,
               fontWeight: FontWeight.w600,
             ),
           ),
           centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            const Center(
+              child: Text(
+                '會員服務 & 隱私權保護條款',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Noto Sans TC',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
