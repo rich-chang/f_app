57038196257b234273750b75ed3f4cd731a2af53
Rich Chang <app.developer.rich@gmail.com>
2022-08-15 15:09:11 +0800

    Add job content

--- lib/components/job_list_content.dart
+++ lib/components/job_list_content.dart
@@ -50,4 +50,60 @@ List<JobList> jobList = [
     index: 10,
     title: "衛生保健",
   ),
+  JobList(
+    index: 11,
+    title: "娛樂業",
+  ),
+  JobList(
+    index: 12,
+    title: "文教機關",
+  ),
+  JobList(
+    index: 13,
+    title: "宗教團體",
+  ),
+  JobList(
+    index: 14,
+    title: "公共事業",
+  ),
+  JobList(
+    index: 15,
+    title: "一般商業",
+  ),
+  JobList(
+    index: 16,
+    title: "服務業",
+  ),
+  JobList(
+    index: 17,
+    title: "家庭管理",
+  ),
+  JobList(
+    index: 18,
+    title: "資訊業",
+  ),
+  JobList(
+    index: 19,
+    title: "治安人員",
+  ),
+  JobList(
+    index: 20,
+    title: "軍人",
+  ),
+  JobList(
+    index: 21,
+    title: "運動人員",
+  ),
+  JobList(
+    index: 22,
+    title: "學生",
+  ),
+  JobList(
+    index: 23,
+    title: "家管",
+  ),
+  JobList(
+    index: 24,
+    title: "其他",
+  ),
 ];
