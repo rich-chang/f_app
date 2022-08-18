871fd8758ace4ee24f52a9389240bd2703244b4f
Rich Chang <app.developer.rich@gmail.com>
2022-08-18 15:37:20 +0800

    Implement splash with testing code
    - Be sure to remove or modify the async/await function for app initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:florish_app/screen/splash.dart';
 import 'package:florish_app/screen/onboarding/onboarding_page.dart';
 import 'package:florish_app/screen/login/login_page.dart';
 import 'package:florish_app/screen/signup/agreement_page.dart';
 import 'package:florish_app/screen/signup/signup_page.dart';
 import 'package:florish_app/screen/signup/signup_flow_name.dart';
+import 'package:florish_app/screen/home/home_page.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
@@ -46,6 +50,28 @@ class TempHome extends StatefulWidget {
 }
 
 class _TempHomeState extends State<TempHome> {
+
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    // This is where you can initialize the resources needed by your app while
+    // the splash screen is displayed.  Remove the following example because
+    // delaying the user experience is a bad design practice!
+    // ignore_for_file: avoid_print
+    print('ready in 3...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('ready in 2...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('ready in 1...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('go!');
+    FlutterNativeSplash.remove();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
