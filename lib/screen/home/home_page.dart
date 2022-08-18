a2d3b0a1311c9c3c997f66445d9ba40544a31e7d
Rich Chang <app.developer.rich@gmail.com>
2022-08-18 13:54:42 +0800

    Implement icon selection on bottom nav bar

--- lib/screen/home/home_page.dart
+++ lib/screen/home/home_page.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:florish_app/screen/home/diary_page.dart';
 import 'package:florish_app/screen/home/explore_page.dart';
 import 'package:florish_app/screen/home/practice_page.dart';
-import 'package:flutter/material.dart';
+import 'package:florish_app/screen/home/profile_page.dart';
+import 'package:florish_app/screen/home/psycho_test_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,11 +15,18 @@ class HomePage extends StatefulWidget {
 
 class _HomeState extends State<HomePage> {
   late int _currentIndex = 0; //預設值
-  final pages = [ExplorePage(), DiaryPage(), PracticePage()];
+  final pages = [
+    ExplorePage(),
+    DiaryPage(),
+    PracticePage(),
+    PsychoTestPage(),
+    ProfilePage()
+  ];
 
   void _onItemTapped(int index) {
     setState(() {
       _currentIndex = index;
+      debugPrint('_currentIndex=$_currentIndex');
     });
   }
 
@@ -25,15 +35,85 @@ class _HomeState extends State<HomePage> {
     return Scaffold(
       body: pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "發現"),
-          BottomNavigationBarItem(icon: Icon(Icons.chat), label: "日記"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle), label: "練習"),
-        ],
+        showUnselectedLabels: true,
+        iconSize: 20,
         currentIndex: _currentIndex,
         selectedItemColor: const Color(0xFF2AC0D4),
+        unselectedItemColor: const Color(0xFF667C85),
         onTap: _onItemTapped,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              label: "發現",
+              icon: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SvgPicture.asset(
+                  'images/botton_nav_explore_default.svg',
+                ),
+              ),
+              activeIcon: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SvgPicture.asset(
+                  'images/botton_nav_explore_selected.svg',
+                ),
+              )),
+          BottomNavigationBarItem(
+            label: "日記",
+            icon: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SvgPicture.asset(
+                'images/botton_nav_diary_default.svg',
+              ),
+            ),
+            activeIcon: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SvgPicture.asset(
+                'images/botton_nav_diary_selected.svg',
+              ),
+            ),
+          ),
+          BottomNavigationBarItem(
+              label: "練習",
+              icon: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SvgPicture.asset(
+                  'images/botton_nav_practice_default.svg',
+                ),
+              ),
+              activeIcon: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SvgPicture.asset(
+                  'images/botton_nav_practice_selected.svg',
+                ),
+              )),
+          BottomNavigationBarItem(
+              label: "心理測驗",
+              icon: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SvgPicture.asset(
+                  'images/botton_nav_psychotest_default.svg',
+                ),
+              ),
+              activeIcon: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SvgPicture.asset(
+                  'images/botton_nav_psychotest_selected.svg',
+                ),
+              )),
+          BottomNavigationBarItem(
+              label: "我的",
+              icon: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SvgPicture.asset(
+                  'images/botton_nav_profile_default.svg',
+                ),
+              ),
+              activeIcon: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SvgPicture.asset(
+                  'images/botton_nav_profile_selected.svg',
+                ),
+              )),
+        ],
       ),
     );
   }
